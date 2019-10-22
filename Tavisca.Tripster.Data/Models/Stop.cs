da8c97d7c4a314e43ce02864d777d6bfec6bd2df
tavisca-ysant <ysant@tavisca.in>
2019-10-22 10:11:02 +0530

    Added data model Stop for storing stop relevant details like ID, Name, Description, Location, Type, Arrival and Departure Date and time, List of places around a stop

---
+++ Tavisca.Tripster.Data/Models/Stop.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tavisca.Tripster.Data.Models
+{
+    public class Stop
+    {
+        public string StopId { get; set; }
+        public string Name { get; set; }
+        public Location Location { get; set; }
+        public long Arrival { get; set; }
+        public long Departure { get; set; }
+        public List<Place> Places { get; set; }
+    }
+}
