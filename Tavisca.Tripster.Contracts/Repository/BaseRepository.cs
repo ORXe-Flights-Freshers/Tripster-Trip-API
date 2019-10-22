c1404e987ebc2f3ede7ad92fd4c315513b637ed8
tavisca-ysant <ysant@tavisca.in>
2019-10-22 12:00:17 +0530

    Added Utility TripCollection for Mocking Trip Collection Database

---
+++ Tavisca.Tripster.Contracts/Repository/BaseRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tavisca.Tripster.Contracts.Repository
+{
+    public class BaseRepository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        
+        public virtual void Add(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity Get(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Guid id, TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
