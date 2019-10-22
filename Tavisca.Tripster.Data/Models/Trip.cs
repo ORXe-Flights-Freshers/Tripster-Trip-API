01aea08fcb02305fd4ad9e38b751623eab0d5efb
tavisca-ysant <ysant@tavisca.in>
2019-10-22 10:14:02 +0530

    Added data model Trip to store TripId, Source, Destination, List of Stops and mileage of the vehicle

---
+++ Tavisca.Tripster.Data/Models/Trip.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tavisca.Tripster.Data.Models
+{
+    public class Trip
+    {
+        public Guid Id { get; set; }
+        public Stop Source { get; set; }
+        public Stop Destination { get; set; }
+        public List<Stop> Stops { get; set; }
+        public int Mileage { get; set; }
+    }
+}
