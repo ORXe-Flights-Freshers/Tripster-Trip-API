332a9dbed3bd426790a0e99518137f50f5c55767
tavisca-ysant <ysant@tavisca.in>
2019-10-22 10:36:59 +0530

    Added contract for basic crud operations for generic entity

---
+++ Tavisca.Tripster.Contracts/Repository/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tavisca.Tripster.Contracts.Repository
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        TEntity Get(Guid id);
+        IEnumerable<TEntity> GetAll();
+        void Add(TEntity entity);
+        void Delete(Guid id);
+        void Update(Guid id, TEntity entity);
+    }
+
+}
